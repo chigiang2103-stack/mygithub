5ab8747f7df0ed205d723d090a3bba9548371112
chigiang2103-stack <chigiang2103@gmail.com>
2025-08-27 22:53:35 +0700

    task1 of lesson4

---
+++ task1L4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n,i;
+    cout<<"nhập n : ";cin>>n;
+    for (i=1;i<=n;i++) {
+        for (int j=1 ;j<=i;j++) {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}

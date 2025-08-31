4f3806b43bdd2b9e144a3f4a851f6407da6e57ce
chigiang2103-stack <chigiang2103@gmail.com>
2025-08-31 22:43:06 +0700

    task4 of lesson4

---
+++ task4L4.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std; 
+int main() {
+    int n ; 
+    cout <<"nhập n : "; cin>> n;
+    int f1,f2,fn;
+        f1=f2=1;
+        cout << "dãy fibo : " <<f1 <<f2 ;
+        while (f1 + f2 < n ) {
+            fn=f2 +f1;
+            f1=f2;
+            cout << fn <<" ";
+            f2=fn;
+        }
+    }
+
+
+
+
+
